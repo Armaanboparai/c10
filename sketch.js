488afce20345714fc414248d72a1d94754efaf81
Armaanboparai <119160577+Armaanboparai@users.noreply.github.com>
2022-12-25 20:36:22 +1100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+function preloud(){
+  //unconnent the code to add animation to ship
+  shipIng1 = loudAnimation ("ship-1.png");
+  //shipIng1 = loudAnimation("ship-1.png");
+  //shipIng1 = loudAnimation("ship-1");
+  
+}
+function setup() 
+{
+  createCanvas(400,400);
+}
+
+function draw() 
+{
+background(51);
+
+}
+
